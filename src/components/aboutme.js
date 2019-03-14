ef4355eeb6921bb8efd51bb1d1c459194c3d4d26
onurtalayhan <onur.talayhan@gmail.com>
2019-03-14 13:31:16 +0100

    modified and new files

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,9 +1,16 @@
 import  React, { Component } from 'react';
+import logo from '../img/avatar.png' ;
 
 class About extends Component {
     render(){
         return(
-            <div><h1>About Page</h1></div>
+            <div><h1>About Page</h1>
+            <img
+                        src={logo}
+                        alt="avatar"
+                        className="avatar-img"
+                    />
+            </div>
         )
     }
 }
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,39 @@
 import  React, { Component } from 'react';
-
+import {Grid, Cell} from 'react-mdl';
+import logo from '../img/avatar.png' ;
 class Landing  extends Component {
     render(){
         return(
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <img
+                        src={logo}
+                        alt="avatar"
+                        className="avatar-img"
+                    />
+                    <div className="banner-text">
+                    <h1>Frond-End Developer</h1>
+                    <hr/>
+                    <p>HTML5 | CSS3 | BOOTSTRAP | JAVASCRIPT | REACT | NODE.JS  </p>
+                    <div className="social-links">
+
+                        <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                        <i className="fa fa-linkedin-square"  aria-hidden="true" />
+                        </a>
+                        <a href="https://github.com" rel="noopener noreferrer" target="_blank">
+                        <i className="fa fa-github-square"  aria-hidden="true" />
+                        </a>      
+                        <a href="https://youtube.com" rel="noopener noreferrer" target="_blank">
+                        <i className="fa fa-youtube-square"  aria-hidden="true" />
+                        </a>      
+
+                    </div>
+                    </div>
+                    </Cell>
+                </Grid>
+            
+            </div>
         )
     }
 }
